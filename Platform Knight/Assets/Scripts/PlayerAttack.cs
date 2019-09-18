ec23f5bd8b32ca78f8f0946d2fd5cc866dae1755
AnaSencar <54727715+AnaSencar@users.noreply.github.com>
2019-09-18 21:15:42 +0200

    Added player movement and attack

---
+++ Platform Knight/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private Transform effectSpawnLocation;
+    [SerializeField] private GameObject effectPrefab;
+
+    private Animator animator;
+    private BoxCollider2D boxCollider;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        boxCollider = GetComponentInChildren<BoxCollider2D>();
+        boxCollider.enabled = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        boxCollider.enabled = false;
+    }
+
+    //animation event
+    public void DoAttack()
+    {
+        boxCollider.enabled = true;
+    }
+
+}
---
+++ Platform Knight/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpDistance = 5f;
+
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    private CapsuleCollider2D capsuleCollider;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        ChangeDirection();
+    }
+
+    private void Move()
+    {
+        float sideToMove = Input.GetAxis("Horizontal");
+        rigidBody.velocity = new Vector2(sideToMove * moveSpeed, rigidBody.velocity.y);
+        animator.SetBool("isWalking", IsPlayerMoving());
+    }
+
+    private void Jump()
+    {
+        if (IsTouchingGround())
+        {
+            rigidBody.velocity += new Vector2(0f, jumpDistance);
+        }
+    }
+
+    private void ChangeDirection()
+    {
+        if (IsPlayerMoving())
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rigidBody.velocity.x), 1f);
+        }
+    }
+
+    private bool IsPlayerMoving()
+    {
+        return Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon;
+    }
+
+    private bool IsTouchingGround()
+    {
+        return capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    }
+
+}
