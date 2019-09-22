f8273492a57c220d7be38372b43a055ed5cf5d2b
AnaSencar <54727715+AnaSencar@users.noreply.github.com>
2019-09-22 02:26:52 +0200

    Added enemy movement and direction change.

---
+++ Platform Knight/Assets/Scripts/EnemyChangeDirection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChangeDirection : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Player")
+        {
+            transform.parent.localScale = new Vector2(-Mathf.Sign(gameObject.GetComponentInParent<Rigidbody2D>().velocity.x), 1f);
+        }
+    }
+}
---
+++ Platform Knight/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 1f;
+    private Rigidbody2D rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localScale.x > 0)
+        {
+            rigidBody.velocity = new Vector2(movementSpeed, 0f);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(-movementSpeed, 0f);
+        }
+    }
+
+
+
+}
