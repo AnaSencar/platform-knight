3cfa7b78692265517e72b1471132ad63cdae5495
AnaSencar <54727715+AnaSencar@users.noreply.github.com>
2019-09-22 19:58:41 +0200

    Added enemy vertical movement

---
+++ Platform Knight/Assets/Scripts/EnemyChangeVerticalDirection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChangeVerticalDirection : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag != "Player")
+        {
+            GetComponentInParent<EnemyMovementVertical>().IsMovingUp = !GetComponentInParent<EnemyMovementVertical>().IsMovingUp;
+        }
+    }
+}
