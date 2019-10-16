3a44e0ba073e1fa981d88733f3471ac012747e3d
AnaSencar <54727715+AnaSencar@users.noreply.github.com>
2019-10-16 17:49:31 +0200

    Removed movement and attacking after entering/exiting portal

--- Platform Knight/Assets/Scripts/Portal.cs
+++ Platform Knight/Assets/Scripts/Portal.cs
@@ -26,13 +26,16 @@ public class Portal : MonoBehaviour
     private IEnumerator SwitchScene()
     {
         DontDestroyOnLoad(gameObject);
+        GameObject player = GameObject.FindWithTag(GameConstants.PLAYER_TAG);
         FaderSystem fader = FindObjectOfType<FaderSystem>();
+        EnableOrDisablePlayerMovementAndAttack(player, false);
         yield return fader.FadeOut();
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
         Portal portalOnWhichToSpawn = GetCorrespondingPortal();
-        UpdatePlayerLocation(portalOnWhichToSpawn);
+        UpdatePlayerLocation(portalOnWhichToSpawn, player);
         yield return new WaitForSeconds(timeToWaitForFader);
         yield return fader.FadeIn();
+        EnableOrDisablePlayerMovementAndAttack(player, true);
         Destroy(gameObject);
     }
 
@@ -52,10 +55,17 @@ public class Portal : MonoBehaviour
         return null;
     }
 
-    private void UpdatePlayerLocation(Portal portalOnWhichToSpawn)
+    private void UpdatePlayerLocation(Portal portalOnWhichToSpawn, GameObject player)
     {
-        GameObject player = GameObject.FindWithTag(GameConstants.PLAYER_TAG);
         player.transform.position = portalOnWhichToSpawn.spawnPoint.transform.position;
+    }
+
+    private void EnableOrDisablePlayerMovementAndAttack(GameObject player, bool shouldEnable)
+    {
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        player.GetComponent<Animator>().SetBool(GameConstants.ISWALKING_ANIM_PAR, false);
+        player.GetComponent<PlayerMovement>().enabled = shouldEnable;
+        player.GetComponent<PlayerAttack>().enabled = shouldEnable;
     }
 
 }
