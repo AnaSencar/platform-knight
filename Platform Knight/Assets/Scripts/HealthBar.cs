2bc2c2d738466bf0add868ae03f8c20f312dc1a8
AnaSencar <54727715+AnaSencar@users.noreply.github.com>
2019-10-17 01:07:40 +0200

    Added health and mana bars

---
+++ Platform Knight/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private Image healthBar;
+    private BasicStats health;
+
+    private void Awake()
+    {
+        healthBar = GetComponent<Image>();
+        health = GetComponentInParent<BasicStats>();
+        if (health == null)
+        {
+            health = FindObjectOfType<PlayerAttack>().GetComponent<BasicStats>();
+        }
+    }
+
+    private void Update()
+    {
+        healthBar.fillAmount = (float)health.CurrentHealth / (float)health.MaxHealth;
+    }
+
+}
---
+++ Platform Knight/Assets/Scripts/ManaBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+{
+    private Image manaBar;
+
+    private void Awake()
+    {
+        manaBar = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        BasicStats mana = FindObjectOfType<PlayerAttack>().GetComponent<BasicStats>();
+        manaBar.fillAmount = mana.CurrentMana / mana.MaxMana;
+    }
+}
